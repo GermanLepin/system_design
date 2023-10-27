10fd7f4e4cdefa9a70898cd7e9ebda340df82e88
German Lepin <germanlepin@Germans-MBP.fritz.box>
2023-10-27 19:50:43 +0200

    system design pub sub pattern

--- pub_sub/main.go
+++ pub_sub/main.go
@@ -9,72 +9,76 @@ import (
 // and communications between them. As indicated in the name, a pub/sub system has two
 // types of entities: Publishers and Subscribers. Publishers are entities that produce
 // messages and publish them to a specific topic, whereas Subscribers are entities that
-// receive messages by subscribing to a specific topic. The pub/sub pattern is often
-// used to decouple different parts of a system in a cloud system. For example, a message
-// queue or message bus can act as a pub/sub system, allowing different parts of a system
-// to communicate without being directly connected to each other. This makes it easier to
-// manage complex systems and improve scalability and reliability.
+// receive messages by subscribing to a specific topic.
 
 func main() {
 	agent := newAgent()
 
-	sub1 := agent.Subscribe("topic1")
-	sub4 := agent.Subscribe("topic1")
+	sub1 := agent.subscribe("topic1")
+	sub2 := agent.subscribe("topic1")
 
-	sub2 := agent.Subscribe("topic2")
-	sub5 := agent.Subscribe("topic2")
+	sub3 := agent.subscribe("topic2")
+	sub4 := agent.subscribe("topic2")
 
-	sub3 := agent.Subscribe("topic3")
-	sub6 := agent.Subscribe("topic3")
-	sub7 := agent.Subscribe("topic3")
-	sub8 := agent.Subscribe("topic3")
+	sub5 := agent.subscribe("topic3")
+	sub6 := agent.subscribe("topic3")
+	sub7 := agent.subscribe("topic3")
+	sub8 := agent.subscribe("topic3")
 
 	agent.wg.Add(11)
-	go agent.Publish("topic1", "message 1")
-	go agent.Publish("topic2", "message 2")
-	go agent.Publish("topic3", "message 3")
+	go agent.publish("topic1", "message 1")
+	go agent.publish("topic2", "message 2")
+	go agent.publish("topic3", "message 3")
 
-	go agent.Read(sub1)
-	go agent.Read(sub2)
-	go agent.Read(sub3)
-	go agent.Read(sub4)
-	go agent.Read(sub5)
-	go agent.Read(sub6)
-	go agent.Read(sub7)
-	go agent.Read(sub8)
+	go agent.read(sub1)
+	go agent.read(sub2)
+	go agent.read(sub3)
+	go agent.read(sub4)
+	go agent.read(sub5)
+	go agent.read(sub6)
+	go agent.read(sub7)
+	go agent.read(sub8)
 
 	agent.wg.Wait()
+	agent.close()
 }
 
-func (a *Agent) Subscribe(topic string) chan string {
+func (a *agent) subscribe(topic string) chan string {
 	ch := make(chan string)
 	a.subs[topic] = append(a.subs[topic], ch)
 
 	return ch
 }
 
-func (a *Agent) Publish(topic string, msg string) {
+func (a *agent) publish(topic string, msg string) {
 	defer a.wg.Done()
 
 	for _, ch := range a.subs[topic] {
 		ch <- msg
-		close(ch)
 	}
 }
 
-func (a *Agent) Read(ch chan string) {
+func (a *agent) read(ch chan string) {
 	defer a.wg.Done()
 
 	fmt.Println(<-ch)
 }
 
-type Agent struct {
+func (a *agent) close() {
+	for _, ch := range a.subs {
+		for _, sub := range ch {
+			close(sub)
+		}
+	}
+}
+
+type agent struct {
 	wg   sync.WaitGroup
 	subs map[string][]chan string
 }
 
-func newAgent() *Agent {
-	return &Agent{
+func newAgent() *agent {
+	return &agent{
 		subs: make(map[string][]chan string),
 	}
 }
